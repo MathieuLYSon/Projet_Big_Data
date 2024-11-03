5fa7e9c3c7d30b2407bdce78a0497a3cf3a03fb1
Mathieu LY WA HOÏ <mathieulyson@gmail.com>
2024-11-03 23:24:28 +0100

    Big Data

---
+++ producer.py
@@ -0,0 +1,175 @@
+#!/usr/bin/python
+
+from pycoingecko import CoinGeckoAPI
+from kafka import KafkaProducer
+import numpy as np
+import json
+import time
+import sys
+
+BROKER = 'localhost:9092'
+TOPIC = 'crypto5'
+list_crypto = [
+    'bitcoin',
+    'ethereum',
+    'binancecoin',
+    'tether',
+    'solana',
+    'cardano',
+    'polkadot',
+    'dogecoin',
+    'litecoin'
+]
+
+def clean_json(json, crypto_name):
+    json_clean = {
+        crypto_name + "_timestamp": [],
+        crypto_name + "_prices": [],
+        crypto_name + "_market_cap": [],
+        crypto_name + "_total_vol": []
+    }
+    js_line = 0
+    for js_col in json:
+        middle_js_line = len(json[js_col][js_line]) // 2
+        for i in json[js_col]:
+            if js_line <= 168:
+                if json[js_col] == json["prices"]:
+                    json_clean[crypto_name + "_timestamp"].extend(json[js_col][js_line][:middle_js_line])
+                    json_clean[crypto_name + "_prices"].extend(json[js_col][js_line][middle_js_line:])
+                elif json[js_col] == json["market_caps"]:
+                    json_clean[crypto_name + "_market_cap"].extend(json[js_col][js_line][middle_js_line:])
+                elif json[js_col] == json["total_volumes"]:
+                    json_clean[crypto_name + "_total_vol"].extend(json[js_col][js_line][middle_js_line:])
+                js_line += 1
+        js_line = 0
+    return json_clean
+
+def producer_bitcoin():
+    bitcoin = cg.get_coin_market_chart_by_id(
+                id="bitcoin",
+                tickers=False,
+                vs_currency='usd',
+                include_market_cap=True,
+                days='7'
+            )
+    clean_bitcoin = clean_json(bitcoin, "BTC")
+    return clean_bitcoin
+
+def producer_ethereum():
+    ethereum = cg.get_coin_market_chart_by_id(
+                id="ethereum",
+                vs_currency='usd',
+                include_market_cap=True,
+                days='7'
+            )
+    clean_ethereum = clean_json(ethereum, "ETH")
+    return clean_ethereum
+
+def producer_binancecoin():
+    binancecoin = cg.get_coin_market_chart_by_id(
+                    id="binancecoin",
+                    vs_currency='usd',
+                    include_market_cap=True,
+                    days='7'
+                )
+    clean_binancecoin = clean_json(binancecoin, "BNB")
+    return clean_binancecoin
+
+def producer_tether():
+    tether = cg.get_coin_market_chart_by_id(
+                    id="tether",
+                    vs_currency='usd',
+                    include_market_cap=True,
+                    days='7'
+                )
+    clean_tether = clean_json(tether, "USDT")
+    return clean_tether
+
+def producer_solana():
+    solana = cg.get_coin_market_chart_by_id(
+                    id="solana",
+                    vs_currency='usd',
+                    include_market_cap=True,
+                    days='7'
+                )
+    clean_solana = clean_json(solana, "SOL")
+    return clean_solana
+
+def producer_cardano():
+    cardano = cg.get_coin_market_chart_by_id(
+                    id="cardano",
+                    vs_currency='usd',
+                    include_market_cap=True,
+                    days='7'
+                )
+    clean_cardano = clean_json(cardano, "ADA")
+    return clean_cardano
+
+def producer_polkadot():
+    polkadot = cg.get_coin_market_chart_by_id(
+                    id="polkadot",
+                    vs_currency='usd',
+                    include_market_cap=True,
+                    days='7'
+                )
+    clean_polkadot = clean_json(polkadot, "DOT")
+    return clean_polkadot
+
+def producer_dogecoin():
+    dogecoin = cg.get_coin_market_chart_by_id(
+                    id="dogecoin",
+                    vs_currency='usd',
+                    include_market_cap=True,
+                    days='7'
+                )
+    clean_dogecoin = clean_json(dogecoin, "DOGE")
+    return clean_dogecoin
+
+def producer_litecoin():
+    litecoin = cg.get_coin_market_chart_by_id(
+                    id="litecoin",
+                    vs_currency='usd',
+                    include_market_cap=True,
+                    days='7'
+                )
+    clean_litecoin = clean_json(litecoin, "LTC")
+    return clean_litecoin
+
+def call_crypto_api():
+    json_full = {}
+    bitcoin = producer_bitcoin()
+    ethereum = producer_ethereum()
+    binancecoin = producer_binancecoin()
+    tether = producer_tether()
+    solana = producer_solana()
+    cardano = producer_cardano()
+    polkadot = producer_polkadot()
+    dogecoin = producer_dogecoin()
+    litecoin = producer_litecoin()
+
+    json_full.update(bitcoin)
+    json_full.update(ethereum)
+    json_full.update(binancecoin)
+    json_full.update(tether)
+    json_full.update(solana)
+    json_full.update(cardano)
+    json_full.update(polkadot)
+    json_full.update(dogecoin)
+    json_full.update(litecoin)
+    return json_full
+
+if __name__ == "__main__":
+    
+    try:
+        producer = KafkaProducer(bootstrap_servers=BROKER)                                                                         
+    except Exception as e:
+        print(f"ERROR --> {e}")
+        sys.exit(1)
+    cg = CoinGeckoAPI()
+    
+    # while True:
+    print("########## Send Data To Kafka: OK ##########")
+    json_full = call_crypto_api()
+    producer.send(TOPIC, json.dumps(json_full).encode('utf-8'))
+    producer.flush()
+    # sleep(5)
---
+++ work/consumer.py
@@ -0,0 +1,170 @@
+#!/usr/bin/python
+
+from pyspark.sql.types import StructType, StructField, FloatType, IntegerType, StringType
+from pyspark.ml.stat import Correlation
+from pyspark.sql import SparkSession
+from pandas.tseries import offsets
+from kafka import KafkaConsumer
+from pymongo import MongoClient
+from datetime import datetime
+import pandas as pd
+import pymongo
+import json
+import time
+
+BROKER = 'kafka:9093'
+TOPIC = 'crypto5'
+list_crypto = [
+    'bitcoin',
+    'ethereum',
+    'binancecoin',
+    'tether',
+    'solana',
+    'cardano',
+    'polkadot',
+    'dogecoin',
+    'litecoin'
+]
+
+def post_in_bdd(spark_df):
+    spark_df.write.format("mongo").mode("append").option("database","crypto").option("collection", "crypto_col").save()
+
+def tot_all_crypto(spark_df):
+    print("\n########## Total all market cap crypto : ##########")
+    total = {}
+    Moyenne_BTC_market_cap = spark_df.agg({'BTC_market_cap': 'mean'}).collect()
+    Moyenne_ETH_market_cap = spark_df.agg({'ETH_market_cap': 'mean'}).show()
+    Moyenne_BNB_market_cap = spark_df.agg({'BNB_market_cap': 'mean'}).show()
+    Moyenne_USDT_market_cap = spark_df.agg({'USDT_market_cap': 'mean'}).show()
+    Moyenne_SOL_market_cap = spark_df.agg({'SOL_market_cap': 'mean'}).show()
+    Moyenne_ADA_market_cap = spark_df.agg({'ADA_market_cap': 'mean'}).show()
+    Moyenne_DOT_market_cap = spark_df.agg({'DOT_market_cap': 'mean'}).show()
+    Moyenne_DOGE_market_cap = spark_df.agg({'DOGE_market_cap': 'mean'}).show()
+    Moyenne_LTC_market_cap = spark_df.agg({'LTC_market_cap': 'mean'}).show()
+    print("moy btc mark cap = ", Moyenne_BTC_market_cap)
+    total.update(total, Moyenne_BTC_market_cap)
+    print(total)
+    # notre_cap_tot = Moyenne_BTC_market_cap.collect() + Moyenne_ETH_market_cap.collect() + Moyenne_BNB_market_cap.collect() + Moyenne_USDT_market_cap.collect() + Moyenne_SOL_market_cap.collect() + Moyenne_ADA_market_cap.collect() + Moyenne_DOT_market_cap.collect() + Moyenne_DOGE_market_cap.collect() + Moyenne_LTC_market_cap.collect()
+    # print(notre_cap_tot)
+
+def corr_btc_market_cap_tot_vol(spark_df):
+    print("\n########## Correlation crypto market cap --> total volume : ##########")
+    Corr_Cap_BTC = spark_df.stat.corr("BTC_market_cap", "BTC_total_vol")
+    print("BTC = ", Corr_Cap_BTC)
+    Corr_Cap_ETH = spark_df.stat.corr("ETH_market_cap", "ETH_total_vol")
+    print("ETH = ", Corr_Cap_ETH)
+    Corr_Cap_BNB = spark_df.stat.corr("BNB_market_cap", "BNB_total_vol")
+    print("BNB = ", Corr_Cap_BNB)
+    Corr_Cap_USDT = spark_df.stat.corr("USDT_market_cap", "USDT_total_vol")
+    print("USDT = ", Corr_Cap_USDT)
+    Corr_Cap_SOL = spark_df.stat.corr("SOL_market_cap", "SOL_total_vol")
+    print("SOL = ", Corr_Cap_SOL)
+    Corr_Cap_ADA = spark_df.stat.corr("ADA_market_cap", "ADA_total_vol")
+    print("ADA = ", Corr_Cap_ADA)
+    Corr_Cap_DOT = spark_df.stat.corr("DOT_market_cap", "DOT_total_vol")
+    print("DOT = ", Corr_Cap_DOT)
+    Corr_Cap_DOGE = spark_df.stat.corr("DOGE_market_cap", "DOGE_total_vol")
+    print("DOGE = ", Corr_Cap_DOGE)
+    Corr_Cap_LTC = spark_df.stat.corr("LTC_market_cap", "LTC_total_vol")
+    print("LTC = ", Corr_Cap_LTC)
+
+
+def corr_btc_price(spark_df):
+    print("########## Correlation BTC --> all crypto : ##########")
+    Corr_BTC = spark_df.stat.corr("BTC_prices", "BTC_prices")
+    print("BTC -> BTC = ", Corr_BTC)
+    Corr_BTC_ETH = spark_df.stat.corr("BTC_prices", "ETH_prices")
+    print("BTC -> ETH = ", Corr_BTC_ETH)
+    Corr_BTC_BNB = spark_df.stat.corr("BTC_prices", "BNB_prices")
+    print("BTC -> BNB = ", Corr_BTC_BNB)
+    Corr_BTC_USDT = spark_df.stat.corr("BTC_prices", "USDT_prices")
+    print("BTC -> USDT = ", Corr_BTC_USDT)
+    Corr_BTC_SOL = spark_df.stat.corr("BTC_prices", "SOL_prices")
+    print("BTC -> SOL = ", Corr_BTC_SOL)
+    Corr_BTC_ADA = spark_df.stat.corr("BTC_prices", "ADA_prices")
+    print("BTC -> ADA = ", Corr_BTC_ADA)
+    Corr_BTC_DOT = spark_df.stat.corr("BTC_prices", "DOT_prices")
+    print("BTC -> DOT = ", Corr_BTC_DOT)
+    Corr_BTC_DOGE = spark_df.stat.corr("BTC_prices", "DOGE_prices")
+    print("BTC -> DOGE = ", Corr_BTC_DOGE)
+    Corr_BTC_LTC = spark_df.stat.corr("BTC_prices", "LTC_prices")
+    print("BTC -> LTC = ", Corr_BTC_LTC)
+
+def minimum(spark_df):
+    print("########## Minimum : ##########")
+    min_BTC = spark_df.agg({"BTC_prices": "min"}).show()
+    min_ETH = spark_df.agg({"ETH_prices": "min"}).show()
+    min_BNB = spark_df.agg({"BNB_prices": "min"}).show()
+    min_USDT = spark_df.agg({"USDT_prices": "min"}).show()
+    min_SOL = spark_df.agg({"SOL_prices": "min"}).show()
+    min_ADA = spark_df.agg({"ADA_prices": "min"}).show()
+    min_DOT = spark_df.agg({"DOT_prices": "min"}).show()
+    min_DOGE = spark_df.agg({"DOGE_prices": "min"}).show()
+    min_LTC = spark_df.agg({"LTC_prices": "min"}).show()
+
+def maximum(spark_df):
+    print("########## Maximum : ##########")
+    max_BTC = spark_df.agg({"BTC_prices": "max"}).show()
+    max_ETH = spark_df.agg({"ETH_prices": "max"}).show()
+    max_BNB = spark_df.agg({"BNB_prices": "max"}).show()
+    max_USDT = spark_df.agg({"USDT_prices": "max"}).show()
+    max_SOL = spark_df.agg({"SOL_prices": "max"}).show()
+    max_ADA = spark_df.agg({"ADA_prices": "max"}).show()
+    max_DOT = spark_df.agg({"DOT_prices": "max"}).show()
+    max_DOGE = spark_df.agg({"DOGE_prices": "max"}).show()
+    max_LTC = spark_df.agg({"LTC_prices": "max"}).show()
+
+def average(spark_df):
+    print("########## Average : ##########")
+    Moyenne_BTC = spark_df.agg({'BTC_prices': 'mean'}).show()
+    Moyenne_ETH = spark_df.agg({'ETH_prices': 'mean'}).show()
+    Moyenne_BNB = spark_df.agg({'BNB_prices': 'mean'}).show()
+    Moyenne_USDT = spark_df.agg({'USDT_prices': 'mean'}).show()
+    Moyenne_SOL = spark_df.agg({'SOL_prices': 'mean'}).show()
+    Moyenne_ADA = spark_df.agg({'ADA_prices': 'mean'}).show()
+    Moyenne_DOT = spark_df.agg({'DOT_prices': 'mean'}).show()
+    Moyenne_DOGE = spark_df.agg({'DOGE_prices': 'mean'}).show()
+    Moyenne_LTC = spark_df.agg({'LTC_prices': 'mean'}).show()
+
+def spark_connect():
+    spark = SparkSession    \
+            .builder    \
+            .master('local')    \
+            .appName('Crypto')  \
+            .config("spark.mongodb.input.uri", "mongodb://root:root@mongo:27017/crypto.*?authSource=admin")  \
+            .config("spark.mongodb.output.uri", "mongodb://root:root@mongo:27017/crypto.*?authSource=admin") \
+            .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.0") \
+            .config("spark-jars-packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.1")  \
+            .getOrCreate()
+    spark.sparkContext
+    return spark
+
+if __name__ == "__main__":
+
+    try:
+        client = MongoClient('mongo', 27017, username = 'root', password = 'root')
+        db_crypto = client.crypto
+        crypto_col = db_crypto.crypto_col
+        calcul_col = db_crypto.calcul_col
+        crypto_col.drop()
+        calcul_col.drop()
+        print("########## Création de la base de données ##########")
+    except:
+        print("Erreur de connexion à MongoDB")
+    consumer = KafkaConsumer(TOPIC, bootstrap_servers=[BROKER], api_version=(2,6,0))
+    spark = spark_connect()
+    for msg in consumer:
+        print("########## Received Data From Producer : OK ##########")
+        df = pd.DataFrame.from_dict(json.loads(msg.value))
+        print("\n########## DataFrame Pandas : ##########\n")
+        print(df)
+        spark_df = spark.createDataFrame(df)
+        post_in_bdd(spark_df)
+        print("\n########## DataFrame Spark : ##########\n")
+        spark_df.show(5, False)
+        average(spark_df)
+        maximum(spark_df)
+        minimum(spark_df)
+        corr_btc_price(spark_df)
+        corr_btc_market_cap_tot_vol(spark_df)
+        tot_all_crypto(spark_df)
